e50898879f36456607131fda6bd38d2f6cac010a
Joao Pedro <joaocruz.oliver17@gmail.com>
2025-06-17 16:46:46 -0300

    Feat:Fazendo as responsividades

--- event/src/components/header/Header.jsx
+++ event/src/components/header/Header.jsx
@@ -2,10 +2,16 @@ import "./Header.css";
 import Logo from "../../assets/img/logoevent.png";
 import { Link } from "react-router-dom";
 import Vector from "../../assets/img/Vector.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = (props) => {
     const [menuAberto, setMenuAberto] = useState(false);
+    const [tipoUsuario, setTipoUsuario] = useState("");
+
+    useEffect(() => {
+        const tipo = localStorage.getItem("tipoUsuario");
+        setTipoUsuario(tipo);
+    }, []);
 
     const toggleMenu = () => {
         setMenuAberto(!menuAberto);
@@ -28,22 +34,33 @@ const Header = (props) => {
                 <nav className="nav_header">
                     <Link className="link_header" to="/Home">Home</Link>
                     <Link className="link_header" to="/ListarEventos">Eventos</Link>
-                    <Link className="link_header" to="/Usuario">Usuarios</Link>
+
+                    {tipoUsuario !== "aluno" && (
+                        <Link className="link_header" to="/Usuario">Usuarios</Link>
+                    )}
+
+                    {tipoUsuario === "admin" && (
+                        <>
+                            <Link className="link_header" to="/TipoEventos">TipoEventos</Link>
+                            <Link className="link_header" to="/TipoUsuario">TipoUsuario</Link>
+                        </>
+                    )}
                 </nav>
 
                 {/* Admin link com imagem */}
                 <nav className="nav_img" style={{ display: props.visibilidade }}>
                     <div className="adm">
-                        <Link to="/Home">{props.tituloHeader}<img src={Vector} alt="portinha" /></Link>
+                        <Link to="/Home">
+                            {props.tituloHeader}
+                            <img src={Vector} alt="portinha" />
+                        </Link>
                     </div>
                 </nav>
 
                 {/* Botão Logar */}
                 <div className="login" style={{ display: props.botao_logar }}>
                     <Link to="/">
-                        <button className="logar">
-                            Logar
-                        </button>
+                        <button className="logar">Logar</button>
                     </Link>
                 </div>
             </div>
@@ -53,7 +70,17 @@ const Header = (props) => {
                 <nav className="nav_mobile">
                     <Link to="/Home" onClick={toggleMenu}>Home</Link>
                     <Link to="/ListarEventos" onClick={toggleMenu}>Eventos</Link>
-                    <Link to="/Usuario" onClick={toggleMenu}>Usuarios</Link>
+
+                    {tipoUsuario !== "aluno" && (
+                        <Link to="/Usuario" onClick={toggleMenu}>Usuarios</Link>
+                    )}
+
+                    {tipoUsuario === "admin" && (
+                        <>
+                            <Link to="/TipoEventos" onClick={toggleMenu}>TipoEventos</Link>
+                            <Link to="/TipoUsuario" onClick={toggleMenu}>TipoUsuario</Link>
+                        </>
+                    )}
                 </nav>
             )}
         </header>
