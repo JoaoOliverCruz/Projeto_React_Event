a72d2632015fe48223e449bed5c76c96365d8edf
Joao Pedro <joaocruz.oliver17@gmail.com>
2025-06-17 16:20:16 -0300

    fazendo as responividades

--- event/src/components/header/Header.jsx
+++ event/src/components/header/Header.jsx
@@ -2,52 +2,62 @@ import "./Header.css";
 import Logo from "../../assets/img/logoevent.png";
 import { Link } from "react-router-dom";
 import Vector from "../../assets/img/Vector.png";
+import { useState } from "react";
 
 const Header = (props) => {
-    return(
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuAberto(!menuAberto);
+    };
+
+    return (
         <header>
             <div className="layout_grid cabecalho">
-                {/* Ao clicar no link, redireciona na tela login */}
-            <Link to="/">
-                <img src= {Logo} alt="Logo do EventPlus"/>
-            </Link>
+                {/* Logo */}
+                <Link to="/Home">
+                    <img src={Logo} alt="Logo do EventPlus" />
+                </Link>
 
-            <nav className="nav_header">
-                {/* <a href="" className="link_header">Home</a>
-                <a href="" className="link_header">Evento</a>
-                <a href="" className="link_header">Usuario</a>
-                <a href="" className="link_header">Contatos</a> */}
-
-                <Link className="link_header" to="/Home">Home</Link>
-                <Link className="link_header" to="/Evento">Eventos</Link>
-                <Link className="link_header" to="/Usuario">Usuarios</Link>
-                <Link className="link_header" to="/Contatos">Contatos</Link>
-            </nav>
-
-            <nav className="nav_img" style={{ display: props.visibilidade}}>
-                <div className="adm">
-                {/* <a href="">{props.tituloHeader}<img src= {Vector} alt="portinha"/></a> */}
-                <Link to="/Home"><img src= {Vector} alt="portinha"/></Link>
-                </div>
-            </nav>
-
-            <div className="login" style={{ display: props.botao_logar }}>
-<<<<<<< HEAD
-                
-                <Link href="" to="/" className="logar">Logar</Link>
-=======
-                {/* <Link href="" to="/">Logar</Link> */}
-                <button className="logar">
-                    Logar
+                {/* Botão hamburguer visível apenas no mobile */}
+                <button className="menu-toggle" onClick={toggleMenu}>
+                    &#9776;
                 </button>
->>>>>>> 3d9cbf82047867dc2e4d52066d5697e752ca2378
+
+                {/* Menu padrão (desktop) */}
+                <nav className="nav_header">
+                    <Link className="link_header" to="/Home">Home</Link>
+                    <Link className="link_header" to="/ListarEventos">Eventos</Link>
+                    <Link className="link_header" to="/Usuario">Usuarios</Link>
+                </nav>
+
+                {/* Admin link com imagem */}
+                <nav className="nav_img" style={{ display: props.visibilidade }}>
+                    <div className="adm">
+                        <Link to="/Home">{props.tituloHeader}<img src={Vector} alt="portinha" /></Link>
+                    </div>
+                </nav>
+
+                {/* Botão Logar */}
+                <div className="login" style={{ display: props.botao_logar }}>
+                    <Link to="/">
+                        <button className="logar">
+                            Logar
+                        </button>
+                    </Link>
+                </div>
             </div>
 
-        </div>
-    </header>
-   
-    )
-}
-
+            {/* Menu Mobile Responsivo */}
+            {menuAberto && (
+                <nav className="nav_mobile">
+                    <Link to="/Home" onClick={toggleMenu}>Home</Link>
+                    <Link to="/ListarEventos" onClick={toggleMenu}>Eventos</Link>
+                    <Link to="/Usuario" onClick={toggleMenu}>Usuarios</Link>
+                </nav>
+            )}
+        </header>
+    );
+};
 
 export default Header;
